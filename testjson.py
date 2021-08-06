566d57607318862a6d8e7c14231522989353f8c5
Omar <omarandres.ormacheahermoza@studenti.polito.it>
2021-08-06 05:22:08 +0200

    'before_starting_actual_labs'

---
+++ testjson.py
@@ -0,0 +1,37 @@
+attrs @ file:///tmp/build/80754af9/attrs_1620827162558/work
+Bottleneck==1.3.2
+certifi==2021.5.30
+click @ file:///home/linux1/recipes/ci/click_1610990599742/work
+click-plugins==1.1.1
+cligj==0.7.2
+cycler==0.10.0
+Fiona==1.8.13.post1
+GDAL==3.0.2
+geopandas @ file:///tmp/build/80754af9/geopandas_1627401391374/work
+joblib @ file:///tmp/build/80754af9/joblib_1613502643832/work
+kiwisolver @ file:///C:/ci/kiwisolver_1612282606037/work
+mapclassify @ file:///tmp/build/80754af9/mapclassify_1627369494639/work
+matplotlib==3.4.1
+mkl-fft==1.3.0
+mkl-random @ file:///C:/ci/mkl_random_1626186184278/work
+mkl-service==2.4.0
+munch==2.5.0
+networkx @ file:///tmp/build/80754af9/networkx_1627459939258/work
+numexpr @ file:///C:/ci/numexpr_1618856738664/work
+numpy @ file:///C:/ci/numpy_and_numpy_base_1626271903025/work
+olefile==0.46
+pandas @ file:///C:/ci/pandas_1626784247794/work
+Pillow @ file:///C:/ci/pillow_1625663293593/work
+pyparsing @ file:///home/linux1/recipes/ci/pyparsing_1610983426697/work
+pyproj @ file:///C:/ci/pyproj_1614278136892/work
+python-dateutil @ file:///tmp/build/80754af9/python-dateutil_1626374649649/work
+pytz @ file:///tmp/build/80754af9/pytz_1612215392582/work
+Rtree @ file:///C:/ci/rtree_1618421009405/work
+scikit-learn @ file:///C:/ci/scikit-learn_1622739500535/work
+scipy @ file:///C:/ci/scipy_1618856128765/work
+Shapely==1.7.1
+sip==4.19.13
+six @ file:///tmp/build/80754af9/six_1623709665295/work
+threadpoolctl @ file:///tmp/build/80754af9/threadpoolctl_1626115094421/work
+tornado @ file:///C:/ci/tornado_1606942392901/work
+wincertstore==0.2
